dfb5b4db4bf1b8b952ddfee685b52b4efe8b1a3c
Angie Zhang <azhang27@terpmail.umd.edu>
2024-05-17 18:08:30 -0400

    feat: Adding code to make food table - Angie

--- script.js
+++ script.js
@@ -1,32 +1,79 @@
 // NUTRITIONIX SEARCH
 
-// Function to make API call
+// Function to make API call to Nutritionix.
 async function retrieveNutritionixData(query) {
-    let appId = "8cef4b7b"
-    let appKey = "0beac0be800a5023af2c26d5491f0248";
-    return fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${query}`, {
-        method: 'GET',
+    const appId = "8cef4b7b"
+    const appKey = "0beac0be800a5023af2c26d5491f0248";
+
+    const response = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
+        method: 'POST',
         headers: {
             'x-app-id': appId,
             'x-app-key': appKey,
             'Content-Type': 'application/json'
-        }
-    })
-        .then(response => response.json());
+        },
+        body: JSON.stringify({
+            "query": query
+        })
+    });
 
+    return await response.json();
 }
 
-
+// Function to search for foods and populate table with food items.
 async function searchNutritionix() {
+
+    let foodItem = document.getElementById('foodItem').value;
     
-    let foodItem = document.getElementById('foodItem').value; 
-    
+    // Prevent function from running if the search bar is empty.
+    if (!foodItem) {
+        return false; 
+    }
+
+    // Clears any previous search.
+    clearTable();
+
+
     let nutritionData = await retrieveNutritionixData(foodItem)
-    let foodName = nutritionData.common[0].food_name;
+    const resultsTable = document.getElementById('nutritionResults');
 
-    console.log("Searching a food gets a list of brands that match it, and a list of common foods: ", nutritionData)
-    console.log("For now I picked and displayed just the first item of the common food: ", nutritionData.common[0])
+    // Populates table with all the components of the food they entered.
+    nutritionData.foods.forEach(item => {
 
-    document.getElementById('nutritionResults').innerHTML = "Food Name: " + foodName;
+        console.log(item.food_name, item.photo.thumb)
+
+        let newRow = document.createElement('tr');
+        let picture = document.createElement('td');
+        let quantity = document.createElement('td');
+        let foodName = document.createElement('td');
+        let numCalories = document.createElement('td');
+
+        picture.innerHTML = `<img src=${item.photo.thumb}></img>`;
+        quantity.innerHTML = item.serving_qty + ' ' + item.serving_unit;
+        foodName.innerHTML = item.food_name;
+        numCalories.innerHTML = item.nf_calories;
+
+        newRow.appendChild(picture);
+        newRow.appendChild(quantity);
+        newRow.appendChild(foodName);
+        newRow.appendChild(numCalories);
+
+        resultsTable.appendChild(newRow);
+    });
+
+    // Table is made visible. 
+    document.getElementById('nutritionResults').style.display = 'block';
+    
+    return false;
 }
 
+
+/* Function to clear the rows in the table except the header if the user wants 
+    to do multiple searches without reloading the page. */
+    function clearTable() {
+        const table = document.getElementById('nutritionResults');
+    
+        for (let i = table.rows.length - 1; i > 0; i--) {
+            table.deleteRow(i);
+        }
+    }
